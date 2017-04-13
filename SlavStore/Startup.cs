363237c6b955c0afda7a07516378c009790ab411
go6koy <go6koy@gmail.com>
2017-04-13 19:37:59 +0300

    Created ASP.NET MVC Project
    Created ASP.NET MVC Project

---
+++ SlavStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SlavStore.Startup))]
+namespace SlavStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
